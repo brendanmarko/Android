f848c629ec0e49c6c455701d333f8b60a71ea3cf
brendan <brendanmarko@gmail.com>
2017-05-01 15:58:37 -0400

    Movement changes

--- GameFiles/app/src/main/java/treadstone/game/GameEngine/Player.java
+++ GameFiles/app/src/main/java/treadstone/game/GameEngine/Player.java
@@ -9,9 +9,9 @@ public class Player extends MovableEntity
 {
     private float                       spanX, spanY, spanZ, x_dir, y_dir;
     private ArrayList<Projectile>       projectiles;
-    private Position                    center;
     private double                      angle_of_movement;
-    private int                         curr_qaudrant;
+
+    private int                         DEBUG = 0;
 
     Player(Context context, Position s, Position m, Position ppm, char t)
     {
@@ -21,44 +21,52 @@ public class Player extends MovableEntity
 
     public void initCenter(Position p)
     {
-        center = p;
         setPosition(p.getX(), p.getY());
     }
 
     public void processMovement(float x_location, float y_location)
     {
-        Log.d("Player/processMove", "Target location: " + x_location + ", " + y_location);
-        Log.d("Player/processMove", "Current Player location: " + getX() + ", " + getY());
+        if (DEBUG == 1)
+        {
+            Log.d("Player/processMove", "Target location: " + x_location + ", " + y_location);
+            Log.d("Player/processMove", "Current Player location: " + getX() + ", " + getY());
+        }
 
         // Set Player to moving
         setMovable();
 
         // Get lengths of sides
         spanX = x_location - getX();
-        spanY = y_location - getY();
+        spanY = y_location - getY() + (0.5fb * getHeight());
         spanZ = (float) Math.sqrt((spanX * spanX) + (spanY * spanY));
 
         // Find angle for movement
-        // angle_of_movement = adjustAngle(calcAngle(Math.toDegrees(Math.asin(Math.abs(getRadians(spanX, spanY, spanZ))))));
         angle_of_movement = calcAngle(Math.toDegrees(Math.asin(Math.abs(getRadians(spanX, spanY, spanZ)))));
-        Log.d("player.movement", "Angle before adjust: " + angle_of_movement);
+
+        if (DEBUG == 1)
+            Log.d("player.movement", "Angle before adjust: " + angle_of_movement);
+
         angle_of_movement = adjustAngle(angle_of_movement);
-        Log.d("player.movement", "Angle after adjust: " + angle_of_movement);
+
+        if (DEBUG == 1)
+            Log.d("player.movement", "Angle after adjust: " + angle_of_movement);
 
         // Apply to Object using move speed
-        calcDisplacement(spanX, spanY, angle_of_movement);
+        calcDisplacement(angle_of_movement);
         boundsCheck(getX(), getY());
     }
 
     @Override
     public void update()
     {
-        //Log.d("Player_update", "update() called in Player");
+        if (DEBUG == 1)
+            Log.d("Player_update", "update() called in Player");
+
         setPosition(getPosition().getX() + x_dir, getPosition().getY() + y_dir);
         boundsCheck(getX(), getY());
     }
 
-    public void calcDisplacement(float x, float y, double angle)
+    public void calcDisplacement(double angle)
     {
 
         if (angle == 0.0d)
@@ -67,10 +75,10 @@ public class Player extends MovableEntity
             y_dir = 0.0f;
         }
 
-        else if (angle == 180.0d)
+        else if (angle == 45.0d)
         {
-            x_dir = 0 - getSpeed();
-            y_dir = 0.0f;
+            y_dir = (0 - getSpeed()) * 0.75f;
+            x_dir = getSpeed() * 0.75f;
         }
 
         else if (angle == 90.0d)
@@ -79,42 +87,64 @@ public class Player extends MovableEntity
             y_dir = 0 - getSpeed();
         }
 
+        else if (angle == 135.0d)
+        {
+            x_dir = (0 - getSpeed()) * 0.75f;
+            y_dir = (0 - getSpeed()) * 0.75f;
+        }
+
+        else if (angle == 180.0d)
+        {
+            x_dir = 0 - getSpeed();
+            y_dir = 0.0f;
+        }
+
+        else if (angle == 225.0d)
+        {
+            x_dir = (0 - getSpeed()) * 0.75f;
+            y_dir = getSpeed() * 0.75f;
+        }
+
         else if (angle == 270.0d)
         {
             x_dir = 0.0f;
             y_dir = getSpeed();
         }
 
-        else
+        else if (angle == 315.0d)
         {
-            x_dir = (float) Math.sin(angle) * getSpeed();
-            y_dir = (float) Math.sqrt(((getSpeed() * getSpeed()) - (x_dir * x_dir)));
-
-            // Factor in directions of x/y span
-            x_dir = Math.copySign(x_dir, x);
-            y_dir = Math.copySign(y_dir, y);
+            x_dir = getSpeed() * 0.75f;
+            y_dir = getSpeed() * 0.75f;
         }
 
+        if (DEBUG == 1)
+            Log.d("Player/calcD", "x_dir, y_dir: " + x_dir + ", " + y_dir);
+
         // Change position
-        Log.d("Player/calcD", "x_dir, y_dir: " + x_dir + ", " + y_dir);
         setPosition(getPosition().getX() + x_dir, getPosition().getY() + y_dir);
     }
 
     public void boundsCheck(float x, float y)
     {
-        Log.d("Player.boundsCheck", "Checking bounds for Player with [X, Y]: " + x + ", " + y);
+        if (DEBUG == 1)
+            Log.d("Player.boundsCheck", "Checking bounds for Player with [X, Y]: " + x + ", " + y);
+
         float new_x = 0.0f;
         float new_y = 0.0f;
 
         if (x < 0.0f)
         {
-            Log.d("Player.boundsCheck", "X < 0");
+            if (DEBUG == 1)
+                Log.d("Player.boundsCheck", "X < 0");
+
             new_x = 0.0f;
         }
 
         else if (x + getWidth() > getXMax())
         {
-            Log.d("Player.boundsCheck", "X > max");
+            if (DEBUG == 1)
+                Log.d("Player.boundsCheck", "X > max");
+
             new_x = getXMax() - getWidth();
         }
 
@@ -125,13 +155,17 @@ public class Player extends MovableEntity
 
         if (y < 0.0f)
         {
-            Log.d("Player.boundsCheck", "Y < 0");
+            if (DEBUG == 1)
+                Log.d("Player.boundsCheck", "Y < 0");
+
             new_y = 0.0f;
         }
 
         else if (y + getHeight() > getYMax())
         {
-            Log.d("Player.boundsCheck", "Y > max");
+            if (DEBUG == 1)
+                Log.d("Player.boundsCheck", "Y > max");
+
             new_y = getYMax() - getHeight();
         }
 
@@ -140,7 +174,9 @@ public class Player extends MovableEntity
             new_y = y;
         }
 
-        Log.d("Player.boundsCheck", "Checking bounds for Player with [X, Y]: " + new_x + ", " + new_y);
+        if (DEBUG == 1)
+            Log.d("Player.boundsCheck", "Checking bounds for Player with [X, Y]: " + new_x + ", " + new_y);
+
         setPosition(new_x, new_y);
 
     }
@@ -171,15 +207,17 @@ public class Player extends MovableEntity
     {
         if (spanX >= 0 && spanY <= 0)
         {
-            Log.d("Player.procMove", "Tapped into Q1");
-            curr_qaudrant = 1;
+            if (DEBUG == 1)
+                Log.d("Player.procMove", "Tapped into Q1");
+
             f += 0.0d;
         }
 
         else if (spanX < 0 && spanY < 0)
         {
-            Log.d("Player.procMove", "Tapped into Q2");
-            curr_qaudrant = 2;
+            if (DEBUG == 1)
+                Log.d("Player.procMove", "Tapped into Q2");
+
             if (Math.abs(spanX) < Math.abs(spanY))
                 f = 180.0d - f;
 
@@ -189,19 +227,21 @@ public class Player extends MovableEntity
 
         else if (spanX <= 0 && spanY >= 0)
         {
-            Log.d("Player.procMove", "Tapped into Q3");
-            curr_qaudrant = 3;
+            if (DEBUG == 1)
+                Log.d("Player.procMove", "Tapped into Q3");
+
             if (Math.abs(spanX) < Math.abs(spanY))
-                f = 270.0d - f;
+                f = 180.0d + f;
 
             else
-                f = 180.0d + f;
+                f = 270.0d - f;
         }
 
         else if (spanX > 0 & spanY > 0)
         {
-            Log.d("Player.procMove", "Tapped into Q4");
-            curr_qaudrant = 4;
+            if (DEBUG == 1)
+                Log.d("Player.procMove", "Tapped into Q4");
+
             if (Math.abs(spanX) < Math.abs(spanY))
                 f = 270.0d + f;
 
