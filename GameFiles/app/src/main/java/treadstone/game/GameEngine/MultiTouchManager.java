e4390cd3089ebfaa2d782faa6b239c4a39124bd6
brendan <brendanmarko@gmail.com>
2017-06-16 13:30:06 -0400

    Player aim/movement 0.7

--- GameFiles/app/src/main/java/treadstone/game/GameEngine/MultiTouchManager.java
+++ GameFiles/app/src/main/java/treadstone/game/GameEngine/MultiTouchManager.java
@@ -1,11 +1,9 @@
 package treadstone.game.GameEngine;
 
 import android.util.Log;
-import android.view.View;
 import android.view.MotionEvent;
-import android.view.View.OnTouchListener;
 
-public class MultiTouchManager implements OnTouchListener
+public class MultiTouchManager
 {
     // Debug info
     private int         DEBUG = 1;
@@ -17,8 +15,6 @@ public class MultiTouchManager implements OnTouchListener
 
     MultiTouchManager(Player p, ViewPort v)
     {
-        if (DEBUG == 1)
-            Log.d(DEBUG_TAG, "MultiTouchMgr created!");
         curr_player = p;
         viewport = v;
         displacementX = 0.0f;
@@ -27,79 +23,23 @@ public class MultiTouchManager implements OnTouchListener
 
     public boolean handleEvent(MotionEvent event)
     {
-        if (DEBUG == 1)
-            Log.d(DEBUG_TAG, "MTM onTouchEvent triggered with: " + event.getAction());
+        Position pointerPos[] = new Position[2];
+        int action = event.getActionMasked();
 
-        int action = event.getAction();
-
-        if (action == MotionEvent.ACTION_DOWN)
+        switch(action)
         {
-            if (DEBUG == 1)
-                Log.d(DEBUG_TAG, "ACTION_DOWN triggered.");
-        }
-
-        else if (action == MotionEvent.ACTION_MOVE)
-        {
-            if (DEBUG == 1)
-                Log.d(DEBUG_TAG, "ACTION_MOVE triggered.");
-        }
-
-        if (action == MotionEvent.ACTION_UP)
-        {
-            if (DEBUG == 1)
-                Log.d(DEBUG_TAG, "ACTION_UP triggered.");
-        }
-
-        if (action == MotionEvent.ACTION_CANCEL)
-        {
-            if (DEBUG == 1)
-                Log.d(DEBUG_TAG, "ACTION_CANCEL triggered.");
-        }
-
-        return true;
-    }
-
-    @Override
-    public boolean onTouch(View v, MotionEvent event)
-    {
-        if (DEBUG == 1)
-            Log.d(DEBUG_TAG, "MTM onTouchEvent triggered");
-
-        // get pointer index from the event object
-        int pointerIndex = event.getActionIndex();
-
-        // get pointer ID
-        int pointerId = event.getPointerId(pointerIndex);
-
-        // get masked (not specific to a pointer) action
-        int maskedAction = event.getActionMasked();
-
-        switch (maskedAction) {
-
-            case MotionEvent.ACTION_DOWN:
-            case MotionEvent.ACTION_POINTER_DOWN:
-            {
-                break;
-            }
-
             case MotionEvent.ACTION_MOVE:
-            {
-                break;
-            }
-
-            case MotionEvent.ACTION_UP:
-
-            case MotionEvent.ACTION_POINTER_UP:
-
-            case MotionEvent.ACTION_CANCEL:
-            {
-                break;
-            }
+                int count = event.getPointerCount();
+                for (int i = 0; i < count; i++)
+                {
+                    pointerPos[i] = new Position(event.getX(i) + displacementX, event.getY(i) + displacementY);
+                    if (DEBUG == 1)
+                        Log.d(DEBUG_TAG, "Co-ordinates: " + pointerPos[i].toString());
+                }
         }
 
         return true;
+        
     }
-
-
 
 }
